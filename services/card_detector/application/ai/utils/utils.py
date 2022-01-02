20787796e425a2ffc378e22645c4ba0943f5e792
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2022-01-03 00:57:06 +0530

    added utils file for image decoding

---
+++ services/card_detector/application/ai/utils/utils.py
@@ -0,0 +1,17 @@
+import base64
+
+
+def decodeImage(imgstring, fileName, image_path):
+    try:
+        imgdata = base64.b64decode(imgstring)
+        with open(image_path+fileName, 'wb') as f:
+            f.write(imgdata)
+            f.close()
+    except BaseException as ex:
+        print(ex)
+
+
+
+def encodeImageIntoBase64(croppedImagePath):
+    with open(croppedImagePath, "rb") as f:
+        return base64.b64encode(f.read())
