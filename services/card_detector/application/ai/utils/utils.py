97d43b802e80b96bcfccf9910e75afb95ee55200
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2022-01-04 00:32:38 +0530

    added minor changes in utils

--- services/card_detector/application/ai/utils/utils.py
+++ services/card_detector/application/ai/utils/utils.py
@@ -4,12 +4,11 @@ import base64
 def decodeImage(imgstring, fileName, image_path):
     try:
         imgdata = base64.b64decode(imgstring)
-        with open(image_path+fileName, 'wb') as f:
+        with open(image_path + fileName, 'wb') as f:
             f.write(imgdata)
             f.close()
     except BaseException as ex:
         print(ex)
-
 
 
 def encodeImageIntoBase64(croppedImagePath):
